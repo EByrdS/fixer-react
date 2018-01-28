00c9a9022bf33c13ea215f6b7989eee75a478363
ebyrds <emmanuel_pajaro@hotmail.com>
2018-01-28 22:24:02 +0000

    Adds right pane containers

---
+++ app/js/containers/Latest.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Latest extends React.Component {
+  render() {
+    return (
+      <div id="latest">
+      <div id="graph-selection">
+        <h2>Graph</h2>
+      </div>
+      <div id="latest-list">
+        <h2>Today</h2>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Latest;
