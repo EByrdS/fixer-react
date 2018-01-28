2723880543ae61ce3ee8d1d6c002bfea1969f0df
ebyrds <emmanuel_pajaro@hotmail.com>
2018-01-28 22:24:41 +0000

    Renders two panes

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo.js';
+import Latest from './containers/Latest.js';
 import Mode from './components/Mode.js';
 import Author from './components/Author.js';
 
 ReactDOM.render(
-  <Logo />,
+  <div>
+  <div id="left-pane">
+  Here will go the graph
+  </div>
+  <div id="right-pane">
+    <Logo />
+    <Latest />
+  </div>
+  </div>,
   document.getElementById('root')
 );
 
