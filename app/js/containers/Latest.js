f522b71d063ff243162640a0605dfe4e1c8f4ce3
ebyrds <emmanuel_pajaro@hotmail.com>
2018-01-30 02:11:31 +0000

    Uses Options components

--- app/js/containers/Latest.js
+++ app/js/containers/Latest.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import Rate from '../components/Rate.js';
+import Options from '../components/Options.js';
 
 class Latest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      base: this.props.base,
+      quote: this.props.quote,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      base: nextProps.base,
+      quote: nextProps.quote,
+    });
+  }
+
+  changeBase(newValue) {
+    this.setState({ base: newValue });
+    this.props.callbackParent({
+      base: newValue,
+      quote: this.state.quote,
+    });
+  }
+
+  changeQuote(newValue) {
+    this.setState({ quote: newValue });
+    this.props.callbackParent({
+      base: this.state.base,
+      quote: newValue,
+    });
+  }
+
   render() {
     return (
       <div id="latest">
       <div id="graph-selection">
         <h2>Graph</h2>
+        <Options selected={this.state.base} callbackParent={(newValue) => this.changeBase(newValue)} />
+        <Options selected={this.state.quote}  callbackParent={(newValue) => this.changeQuote(newValue)} />
       </div>
       <div id="latest-list">
         <h2>Latest</h2>
