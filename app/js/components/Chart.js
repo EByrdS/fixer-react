95ad7209b2512b996a272aaa178379d44c0653d7
ebyrds <emmanuel_pajaro@hotmail.com>
2018-01-31 01:26:29 +0000

    Create chart on asynchronous calls

--- app/js/components/Chart.js
+++ app/js/components/Chart.js
@@ -7,8 +7,10 @@ charts(FusionCharts);
 
 var myDataSource = {
   chart: {
-    xAxisName: "Time",
+    xAxisName: "Date",
     yAxisName: "Value",
+    yAxisMaxValue: "",
+    palette: "3",
   },
   data: []
 };
@@ -21,29 +23,67 @@ class Chart extends React.Component {
       base: props.base,
       quote: props.quote,
       days: props.days,
-      filterSouce: '',
+      dataSource: myDataSource,
+      dataArray: [],
     };
   }
 
-  componentWillReceiveProps() {
+  componentWillReceiveProps(newProps) {
     this.setState({
-      filterSouce: 'btn_update_data'
+      base: newProps.base,
+      quote: newProps.quote,
+      days: newProps.days
+    }, this.updateChart());
+  }
+
+  addChartObject(chartObject) {
+    var updatedArray = this.state.dataArray;
+    updatedArray.unshift(chartObject);
+    var intermediateData = Object.assign({}, this.state.dataSource);
+    intermediateData.chart.yAxisMaxValue = parseFloat(chartObject.value, 10) * 2;
+    intermediateData.data = updatedArray;
+    this.setState({
+      dataSource: intermediateData,
+      dataArray: updatedArray,
     });
   }
 
-  componentDidMount() {
-    console.log("Will fetch the data from fixer.io");
+  recursiveFetch(daysForward) {
+    var latestDate = new Date(this.state.latestDate);
+    latestDate.setDate(latestDate.getDate() - (this.state.days - daysForward));
+    fetch("https://api.fixer.io/" +
+        latestDate.getFullYear() + "-" +
+        ("0" + (latestDate.getMonth() + 1)).slice(-2) + "-" +
+        ("0" + latestDate.getDate()).slice(-2) +
+        "?symbols=" + this.state.quote + "&base=" + this.state.base)
+      .then(res => res.json())
+      .then((result) => {
+        var chartObject = {
+          label: result.date,
+          value: result.rates[this.state.quote],
+        };
+        this.addChartObject(chartObject, daysForward);
+        if (daysForward > 1) {
+          this.recursiveFetch(daysForward - 1);
+        }
+      });
+  }
+
+  updateChart() {
     fetch("https://api.fixer.io/" +
         "latest?symbols=" + this.state.quote + "&base=" + this.state.base)
       .then(res => res.json())
       .then((result) => {
-        console.log("Got the data from fixer.io");
-        myDataSource.data.push({ label: result.date, value: result.rates[this.state.quote] });
         this.setState({
-          filterSouce: 'btn_update_data',
+          latestDate: result.date,
+          dataArray: [],
         });
-        console.log(myDataSource);
+        this.recursiveFetch(this.state.days);
       });
+  }
+
+  componentDidMount() {
+    this.updateChart();
   }
 
   render() {
@@ -53,9 +93,7 @@ class Chart extends React.Component {
       width: "400",
       height: "300",
       dataFormat: "JSON",
-      dataSource: myDataSource,
-      eventTarget: this.state.eventTarget,
-      impactedBy: ['btn-update-data']
+      dataSource: this.state.dataSource,
     };
 
     return (
